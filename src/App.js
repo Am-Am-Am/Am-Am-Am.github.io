21905fe10e69fadef184265132965f4ac3c121c0
Amur <amur_almukhametov@mail.ru>
2021-10-25 13:18:19 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Settings from "./components/Settings/Settings";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = (props) => {
+
+    /*let postsData = [
+      {id: 1, message: 'Привет'}
+  ];*/
+
+    /* let dialogsData = [
+        {id: 1, name: 'Oxxxymiron1'},
+        {id: 2, name: 'Oxxxymiron2'},
+        {id: 3, name: 'Oxxxymiron3'},
+        {id: 4, name: 'Oxxxymiron4'}
+    ]
+
+    let messageData = [
+        {id: 1, message: 'Oxxxymiron1'},
+        {id: 2, message: 'Oxxxymiron2'},
+        {id: 3, message: 'Oxxxymiron3'},
+        {id: 4, message: 'Oxxxymiron4'}
+    ]*/
+
+    return (
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                {/*<Profile />*/}
+                <div className="app-wrapper-content">
+                    <Route exact path="/dialogs" render={() => <Dialogs dialogsData={props.dialogsData} messageData={props.messageData}/>}/>
+                    <Route exact path="/profile" render={() => <Profile postsData={props.postsData}/>}/>
+                    <Route exact path="/news" render={() => <News />}/>
+                    <Route exact path="/music" render={() => <Music />}/>
+                    <Route exact path="/settings" render={() => <Settings />}/>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
