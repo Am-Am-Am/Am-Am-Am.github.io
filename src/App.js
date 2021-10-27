63eb617855ef9c9066fa9381929de52a4ee926f0
Amur <amur_almukhametov@mail.ru>
2021-10-27 17:22:44 +0500

    4

--- src/App.js
+++ src/App.js
@@ -13,36 +13,18 @@ import Settings from "./components/Settings/Settings";
 
 const App = (props) => {
 
-    /*let postsData = [
-      {id: 1, message: 'Привет'}
-  ];*/
-
-    /* let dialogsData = [
-        {id: 1, name: 'Oxxxymiron1'},
-        {id: 2, name: 'Oxxxymiron2'},
-        {id: 3, name: 'Oxxxymiron3'},
-        {id: 4, name: 'Oxxxymiron4'}
-    ]
-
-    let messageData = [
-        {id: 1, message: 'Oxxxymiron1'},
-        {id: 2, message: 'Oxxxymiron2'},
-        {id: 3, message: 'Oxxxymiron3'},
-        {id: 4, message: 'Oxxxymiron4'}
-    ]*/
-
     return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
-                {/*<Profile />*/}
                 <div className="app-wrapper-content">
-                    <Route exact path="/dialogs" render={() => <Dialogs dialogsData={props.dialogsData} messageData={props.messageData}/>}/>
-                    <Route exact path="/profile" render={() => <Profile postsData={props.postsData}/>}/>
-                    <Route exact path="/news" render={() => <News />}/>
-                    <Route exact path="/music" render={() => <Music />}/>
-                    <Route exact path="/settings" render={() => <Settings />}/>
+                    <Route exact path="/dialogs"
+                           render={() => <Dialogs dialogsData={props.dialogsData} messageData={props.messageData}/>}/>
+                    <Route exact path="/profile" render={() => <Profile postsData={props.postsData} addPost={props.addPost} updateNewPostText={props.updateNewPostText}/>}/>
+                    <Route exact path="/news" render={() => <News/>}/>
+                    <Route exact path="/music" render={() => <Music/>}/>
+                    <Route exact path="/settings" render={() => <Settings/>}/>
                 </div>
             </div>
         </BrowserRouter>
